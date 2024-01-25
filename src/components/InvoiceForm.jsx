22d1a063b91ac373f032aea1200937da6f23de71
creytas <creytas@gmail.com>
2024-01-25 02:54:22 +0100

    style: set inputs controls

--- src/components/InvoiceForm.jsx
+++ src/components/InvoiceForm.jsx
@@ -1,68 +1,142 @@
 // components/InvoiceForm.js
-import React, { useState } from 'react';
-import { html2pdf } from 'html2pdf.js';
+import React, { useState, useEffect } from "react";
+// import html2pdf from "html2pdf.js";
 
 const InvoiceForm = () => {
-  const [articles, setArticles] = useState([{ name: '', quantity: 1, price: 0 }]);
+  const [articles, setArticles] = useState([
+    { name: "", quantity: 1, price: 0 },
+  ]);
   const [taxRate, setTaxRate] = useState(0);
   const [discount, setDiscount] = useState(0);
+  const [isPrintButtonDisabled, setPrintButtonDisabled] = useState(true);
+  const [isDeleteButtonHidden, setDeleteButtonHidden] = useState(false);
 
-  const handlePrint = () => {
-    const element = document.getElementById('invoice-form');
-    html2pdf(element);
+  const handleShowInvoice = () => {
+    const invoiceData = {
+      articles,
+      taxRate,
+      discount,
+      total: calculateTotal(),
+    };
+
+    // Affiche les données JSON dans une boîte de dialogue
+    alert(JSON.stringify(invoiceData, null, 2));
   };
 
+  //   const handlePrint = () => {
+  //     const element = document.getElementById("invoice-form");
+  //     html2pdf(element);
+  //   };
+
   const handleAddArticle = () => {
-    setArticles([...articles, { name: '', quantity: 1, price: 0 }]);
+    setArticles([...articles, { name: "", quantity: 1, price: 0 }]);
   };
 
   const handleRemoveArticle = (index) => {
     const updatedArticles = [...articles];
     updatedArticles.splice(index, 1);
     setArticles(updatedArticles);
+    setDeleteButtonHidden(updatedArticles.length === 1);
+    const isAnyArticleNameEmpty = updatedArticles.some(
+      (article) => article.name.trim() === ""
+    );
+    setPrintButtonDisabled(isAnyArticleNameEmpty);
   };
 
   const handleArticleChange = (index, field, value) => {
     const updatedArticles = [...articles];
     updatedArticles[index][field] = value;
     setArticles(updatedArticles);
+    const isAnyArticleNameEmpty = updatedArticles.some(
+      (article) => article.name.trim() === ""
+    );
+    setPrintButtonDisabled(isAnyArticleNameEmpty);
   };
 
   const calculateTotal = () => {
-    const subtotal = articles.reduce((acc, article) => acc + article.quantity * article.price, 0);
+    const subtotal = articles.reduce(
+      (acc, article) => acc + article.quantity * article.price,
+      0
+    );
     const totalBeforeTaxAndDiscount = subtotal * (1 - discount / 100);
     const total = totalBeforeTaxAndDiscount * (1 + taxRate / 100);
     return total.toFixed(2);
   };
+
+  const formatNumberInput = (value) => {
+    // Enlever les zéros à gauche des nombres entiers
+    const formattedValue = value.replace(/^0+(?=\d)/, "");
+    return formattedValue === "" ? "0" : formattedValue; // Éviter une chaîne vide
+  };
+
+  const validateNumberInput = (value) => {
+    // Vérifiez si la valeur est un nombre positif
+    return /^\d*\.?\d+$/.test(value) && parseFloat(value) >= 0;
+  };
+
+  useEffect(() => {
+    // Mettre à jour la validation du bouton d'impression
+    const isAnyArticleNameEmpty = articles.some(
+      (article) => article.name.trim() === ""
+    );
+    setPrintButtonDisabled(isAnyArticleNameEmpty);
+  }, [articles]);
 
   return (
     <div>
       <form id="invoice-form">
         {/* ... autres champs de la facture */}
         <h2>Articles</h2>
+        {/* <div className="grid grid-cols-4 gap-4">
+          <div className="font-bold">Nom de l'article</div>
+          <div className="font-bold">Quantité</div>
+          <div className="font-bold">Prix unitaire</div>
+          <div className="font-bold">Actions</div>
+        </div> */}
         {articles.map((article, index) => (
           <div key={index}>
             <input
               type="text"
               value={article.name}
-              onChange={(e) => handleArticleChange(index, 'name', e.target.value)}
+              onChange={(e) =>
+                handleArticleChange(index, "name", e.target.value)
+              }
               placeholder="Nom de l'article"
+              required
             />
             <input
-              type="number"
+              type="text"
               value={article.quantity}
-              onChange={(e) => handleArticleChange(index, 'quantity', e.target.value)}
+              onChange={(e) =>
+                handleArticleChange(
+                  index,
+                  "quantity",
+                  validateNumberInput(e.target.value)
+                    ? formatNumberInput(e.target.value)
+                    : article.quantity
+                )
+              }
               placeholder="Quantité"
             />
             <input
-              type="number"
+              type="text"
               value={article.price}
-              onChange={(e) => handleArticleChange(index, 'price', e.target.value)}
+              onChange={(e) =>
+                handleArticleChange(
+                  index,
+                  "price",
+                  validateNumberInput(e.target.value)
+                    ? formatNumberInput(e.target.value)
+                    : article.price
+                )
+              }
               placeholder="Prix unitaire"
             />
-            <button type="button" onClick={() => handleRemoveArticle(index)}>
-              Supprimer
-            </button>
+            {index > 0 && (
+              <button type="button" onClick={() => handleRemoveArticle(index)}>
+                Supprimer
+              </button>
+            )}
           </div>
         ))}
         <button type="button" onClick={handleAddArticle}>
@@ -72,18 +146,30 @@ const InvoiceForm = () => {
         <div>
           <label>Taux de taxe (%)</label>
           <input
-            type="number"
+            type="text"
             value={taxRate}
-            onChange={(e) => setTaxRate(e.target.value)}
+            onChange={(e) =>
+              setTaxRate(
+                validateNumberInput(e.target.value)
+                  ? formatNumberInput(e.target.value)
+                  : taxRate
+              )
+            }
             placeholder="Taux de taxe"
           />
         </div>
         <div>
           <label>Remise (%)</label>
           <input
-            type="number"
+            type="text"
             value={discount}
-            onChange={(e) => setDiscount(e.target.value)}
+            onChange={(e) =>
+              setDiscount(
+                validateNumberInput(e.target.value)
+                  ? formatNumberInput(e.target.value)
+                  : discount
+              )
+            }
             placeholder="Remise"
           />
         </div>
@@ -91,7 +177,17 @@ const InvoiceForm = () => {
           <strong>Total: {calculateTotal()} €</strong>
         </div>
       </form>
-      <button onClick={handlePrint}>Imprimer en PDF</button>
+      <button
+        onClick={handleShowInvoice}
+        className={`mr-4 p-2 ${
+          isPrintButtonDisabled
+            ? "bg-gray-300 cursor-not-allowed"
+            : "bg-blue-500 text-white"
+        }`}
+        disabled={isPrintButtonDisabled}
+      >
+        Imprimer en PDF
+      </button>
     </div>
   );
 };
