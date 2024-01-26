7a1c1935de669d6c80f67f8702008315a65a4045
creytas <creytas@gmail.com>
2024-01-26 22:27:57 +0100

    refactor: make the exportToXLSX fx reusable

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,20 +1,27 @@
 import ExcelJS from "exceljs";
 
-const exportToXLSX = () => {
+const exportToXLSX = (data, title) => {
   const today = new Date().toLocaleDateString().replace(/\//g, "-");
   const workbook = new ExcelJS.Workbook();
-  const worksheet = workbook.addWorksheet(`DailyInvoices_${today}`);
+  const worksheet = workbook.addWorksheet(
+    `${
+      title === "DailyInvoices"
+        ? `DailyInvoices_${today}`
+        : `Invoice_${invoiceId}`
+    }`
+  );
   worksheet.columns = [
     { header: "Date", key: "invoiceDate", width: 20 },
     { header: "Invoice Id", key: "invoiceId", width: 20 },
     { header: "Articles", key: "articles", width: 40 },
     { header: "Tax Rate", key: "taxRate", width: 15 },
     { header: "Discount", key: "discount", width: 15 },
+    { header: "Sous-Total", key: "subTotal", width: 15 },
     { header: "Total", key: "total", width: 15 },
   ];
 
-  dailyInvoices.forEach((invoice) => {
-    worksheet.addRow(invoice);
+  data.forEach((item) => {
+    worksheet.addRow(item);
   });
 
   workbook.xlsx.writeBuffer().then((buffer) => {
